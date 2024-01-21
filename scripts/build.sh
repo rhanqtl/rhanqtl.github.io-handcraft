5fd1e4e1d57d9a70ea6e3ed81b16bf5ac413b8be
rhanqtl <my@rhanqtl.com>
2024-01-21 22:47:23 +0800

    update build script & gitignore

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+set -x
+
 find -name '*.ad' | xargs -n1 bash scripts/cvt.sh
-mkdir build
+
+if [[ -d build ]]; then
+  rm -rf build/**
+else
+  mkdir build
+fi
 cp -r content/** build
+find -name '*.ad' -or -name 'a.out' -or -name '*.exe' | xargs rm
 
-git checkout gh-pages
+# git checkout gh-pages
 
-cp build/** .
-find -name '*.ad' | xargs rm
+# cp build/** .
