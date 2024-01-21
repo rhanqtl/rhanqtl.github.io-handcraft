9fa69620008b6b05fd7ba4235216bd3d861ea1fe
rhanqtl <my@rhanqtl.com>
2024-01-21 22:37:18 +0800

    reorg

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
 find -name '*.ad' | xargs -n1 bash scripts/cvt.sh
+mkdir build
+cp -r content/** build
+
+git checkout gh-pages
+
+cp build/** .
+find -name '*.ad' | xargs rm
